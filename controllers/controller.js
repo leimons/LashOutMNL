3c4c0f9e60f8696386ef2a4929737d5c5e41d9d1
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-10-24 02:06:09 +0800

    Appointments DB added

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const db = require("../database/models/db");
 const Products = require("../database/models/Products");
+const Appointments = require("../database/models/Appointments");
 
 const controller = {
     getHomePage: function (req,res){
@@ -10,9 +11,18 @@ const controller = {
             Description: "Curl Your Lashes",
             Price: 500
         }
+        var TempAppointment = {
+            refNum: 100021,
+            ClientName: "Joe Mama",
+            ClientInfo: "Covid Negative",
+            PaymentStatus: false
+        }
+        db.insertOne(Appointments, TempAppointment, function(){
+
+        });
         db.insertOne(Products, TempProduct, (err)=> {
             res.render ('index');
-        })
+        });
     }
 }
 
