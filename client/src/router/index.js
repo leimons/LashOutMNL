f7bf6fb17af03149114e5c9964b1a41bdc11a91c
nicapos <annikaposadas22@gmail.com>
2022-11-13 17:12:11 +0800

    Minor bug fix

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -14,7 +14,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
+  hashbang: false,
   routes
 })
 
