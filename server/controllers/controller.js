ff64ccc8d365f94fca2d5e52be0602df05eeffac
nicapos <annikaposadas22@gmail.com>
2022-11-14 09:44:16 +0800

    Update API

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -4,39 +4,48 @@ const Appointments = require("../database/models/Appointments");
 
 const controller = {
 
-    getServices: async function (req, res) {
-        const projection = "-_id Name Category Description Duration Price"; // "-_id" excludes id field from data
-        const services = await Products.find({}, projection);
+    getServices: function (req, res) {
+        var category = req.query.category;
 
-        res.setHeader('Content-Type', 'application/json');
-        res.send(services);
+        var projection = "-_id Name Category Description Duration Price"; // "-_id" excludes id field from data
+        db.findMany(Products, {}, projection, (err, result) => {
+            res.setHeader('Content-Type', 'application/json');
+
+            if ( category ) {
+                var filteredServices = result.filter((Category) => { return Category == category })
+                res.send(filteredServices);
+            } else {
+                var services = result;
+                res.send(services);
+            }
+        })
     },
 
     getCategories: (req, res) => {
         const categories = [
             {
                 name: "Lashes",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", /* TODO: Update description */
                 priceDescription: "starts at ₱550.00"
             },
             {
                 name: "Lashes Retouch",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", /* TODO: Update description */
                 priceDescription: "starts at ₱500.00"
             },
             {
                 name: "Nails",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", /* TODO: Update description */
                 priceDescription: "starts at ₱150.00"
             },
             {
                 name: "Brows",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", /* TODO: Update description */
                 priceDescription: "starts at ₱4,000.00"
             },
             {
                 name: "Brows Retouch",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", /* TODO: Update description */
                 priceDescription: "starts at ₱1,500.00"
             }
         ]
