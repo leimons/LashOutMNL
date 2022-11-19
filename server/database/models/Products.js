a208085ed1f4c3d5f5675a2a67cd4cf642da6932
nicapos <annikaposadas22@gmail.com>
2022-11-19 11:23:59 +0800

    Update API
    - add columns Subcategory, OnSale, and SalePrice to Product model/db
    - remove categories API

--- server/database/models/Products.js
+++ server/database/models/Products.js
@@ -3,9 +3,12 @@ const mongoose = require ('mongoose');
 const ProductSchema = new mongoose.Schema({
     Name: String,
     Category: String,
+    Subcategory: String,
     Description: String,
     Duration: String,
-    Price: Number
+    Price: Number,
+    OnSale: Boolean,
+    SalePrice: Number
 })
 
 const Product = mongoose.model('Product', ProductSchema);
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,12 +4,6 @@ const controller = require(`../controllers/controller.js`);
 const app = express();
 
 /*
-    URL Path: `/api/categories`
-    Returns list of categories as JSON
-*/
-app.get(`/categories`, controller.getCategories)
-
-/*
     URL Path: `/api/services`
     Returns list of services as JSON
 */
