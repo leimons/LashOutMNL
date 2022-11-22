063f04fbfe1220fd97da24285bc2b1cf2d21cfab
Gabe Piano <43084571+Poije@users.noreply.github.com>
2022-11-22 15:41:45 +0800

    PlaceHolderPage for Inclusions

---
+++ client/src/components/InclusionsCard.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'InclusionsCard',
+
+        DataTransfer() {
+            return {
+                chosenInclusions: []
+            }
+        }
+    }
+</script>
+<template>
+        <input type="checkbox" value="Inclusion1" v-model="chosenInclusions" id="Inclusion1"/>
+        <label for="Inclusion1">"Inclusion1"</label> <br><br>
+</template>
