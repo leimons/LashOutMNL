04b82429a633b60f8f7ab02495740431854754a3
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-12-03 18:41:11 +0800

    Update adminfunctions.js

--- client/src/adminfunctions.js
+++ client/src/adminfunctions.js
@@ -6,19 +6,26 @@ class Service {
 		console.log("attempting to login")
 		return axios.post(url,{password});
 	}
+	/*
     static addService (s_name, s_category, s_subcat, s_dur, s_prc, s_desc, s_onS, s_sale) {
         var url = "http://localhost:3000/api/addservice"
         console.log ("The Value is: " + value)
         return axios.post(url, {
-            product: value,
-            AmountDue: price
+            Service: s_name,
+			Category: s_category,
+			Subcategory: s_subcat,
+			Duration: s_dur,
+			Price: s_prc,
+			Description: s_desc,
+			OnSale: s_onS,
+			SalePrice: s_sale
         });
     }
     static deleteService (s_name){
 		var url = "http://localhost:3000/api/deleteservice"
 		console.log(s_name + "has been deleted")
 		return axios.post(url,{s_name});
-	}
+	}*/
 }
 
-export default Appointment;
+export default Service;
