da787ba748007c557108810db9a73b229158fbd7
nicapos <annikaposadas22@gmail.com>
2022-12-12 01:48:04 +0800

    Revise customer form

--- client/src/views/booking/CheckoutView.vue
+++ client/src/views/booking/CheckoutView.vue
@@ -3,6 +3,7 @@
     
     import InclusionSubview from '@/views/booking/InclusionSubview.vue';
     import ScheduleSubview from '@/views/booking/ScheduleSubview.vue';
+    import CustomerSubview from '@/views/booking//CustomerSubview.vue';
     import dbFunctions from '@/dbFunctions.js';
 
     const SECTION_ID = ["#inclusions-card", "#schedules-card", "#info-card"];
@@ -10,18 +11,13 @@
     export default {
         name: 'CheckoutView',
         title: 'Checkout | LashOut MNL',
-        components: { MilestoneCard, InclusionSubview, ScheduleSubview },
+        components: { MilestoneCard, InclusionSubview, ScheduleSubview, CustomerSubview },
         data() {
             return {
                 currentStep: 1,
                 scrollMargin: 0,
                 isLoading: true,
    
-                customer: {
-                    name: '',
-                    email: '',
-                    contact: ''
-                },
                 proofOfPayment: null,
                 hasSetAppointment: false
             }
@@ -108,40 +104,7 @@
         <ScheduleSubview id="schedules-card" :step=2 :currentStep="currentStep" />
 
         <!-- Step 3: Enter information -->
-        <MilestoneCard :step=3 :currentStep="currentStep">
-            <template #heading>
-                Personal Information
-            </template>
-            <template #content>
-                <div class="flex-col" id="info-card">
-                    
-                    <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie est at ante luctus fringilla. Morbi venenatis turpis sapien, sit amet euismod metus fringilla ut.</i><!-- TODO: Add description/instructions for enter information -->
-                
-                    <!-- Customer Information -->
-                    <div style="width: 100%;">
-                        <div class="flex-row">
-                            <label for="name">Name</label>
-                            <input type="text" id="name" v-model="customer.name" required />
-                        </div>
-                        <div class="flex-row">
-                            <label for="email">Email</label>
-                            <input type="email" id="email" v-model="customer.email" required />
-                        </div>
-                        <div class="flex-row">
-                            <label for="phone">Phone Number</label>
-                            <input type="tel" id="phone" name="phone" v-model="customer.contact" required />
-                        </div>
-                    </div>
-
-                    <div class="flex-row">
-                        <button class="small grey" v-show="(currentStep == 3)" @click="prevStep">Back</button>
-                        <button class="small dark next" v-show="(currentStep == 3)  && completedStep(3)" @click="nextStep">Next</button>
-                    </div>
-
-                </div>
-            </template>
-        </MilestoneCard>
-
+        <CustomerSubview id="info-card" :step=3 :currentStep="currentStep" />
 
         <!-- Step 4: Payment Information -->
         <MilestoneCard :step=4 :currentStep="currentStep">
---
+++ client/src/views/booking/CustomerSubview.vue
@@ -0,0 +1,61 @@
+<script>
+    import MilestoneCard from '@/components/Booking/MilestoneCard.vue';
+
+    export default {
+        name: 'CustomerSubview',
+        components: { MilestoneCard },
+        props: {
+            step: Number,
+            currentStep: Number,
+            id: String
+        },
+        data() {
+            return {
+                name: '',
+                email: '',
+                contact: ''
+            }
+        },
+        computed: {
+            isActiveStep() {
+                return this.step == this.currentStep
+            }
+        }
+    }
+</script>
+
+<template>
+    <MilestoneCard :step=3 :currentStep="currentStep">
+        <template #heading>
+            Personal Information
+        </template>
+        <template #content>
+            <div class="flex-col" :id="id">
+                
+                <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie est at ante luctus fringilla. Morbi venenatis turpis sapien, sit amet euismod metus fringilla ut.</i><!-- TODO: Add description/instructions for enter information -->
+            
+                <!-- Customer Information -->
+                <div style="width: 100%;">
+                    <div class="flex-row">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" v-model="name" required />
+                    </div>
+                    <div class="flex-row">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" v-model="email" required />
+                    </div>
+                    <div class="flex-row">
+                        <label for="phone">Phone Number</label>
+                        <input type="tel" id="phone" name="phone" v-model="contact" required />
+                    </div>
+                </div>
+
+                <div class="flex-row">
+                    <button class="small grey" v-show="isActiveStep" @click="prevStep">Back</button>
+                    <button class="small dark next" v-show="(currentStep == 3)  && completedStep(3)" @click="nextStep">Next</button>
+                </div>
+
+            </div>
+        </template>
+    </MilestoneCard>
+</template>
