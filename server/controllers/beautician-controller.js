4b86e9440037a34001037aeb4310d73c594adc0d
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-12-12 21:04:09 +0800

    Update beautician-controller.js

--- server/controllers/beautician-controller.js
+++ server/controllers/beautician-controller.js
@@ -5,7 +5,8 @@ const bea_controller = {
 	bea_add: function(req, res) {
         console.log("inserting: " + req.query.Name);
         db.insertOne(Beautician, req.query, function(){}); 
-		console.log(req.query.Name + "added");
+	console.log(req.query.Name + "added");
+	res.status(201).send();
     	},
 	
 	bea_delete: function (req, res) {
@@ -13,14 +14,16 @@ const bea_controller = {
 	var beauticianName = req.query.Name
         console.log("deleting: " + beauticianName);
         db.deleteOne(Beautician,{_id:ref}, function(){});
-		console.log("Beautician " + beauticianName + " has been removed");
+	console.log("Beautician " + beauticianName + " has been removed");
+	res.status(201).send();
     	},
 	
 	bea_schedule_change: function(req, res) {
 	var ref = req.query._id;
         console.log("updating: "+req.query.Name);
         db.updateOne({_id:ref},{Name:req.query.name});
-		console.log(s_name + " updated");
+	console.log(s_name + " updated");
+	res.status(201).send();
 	}
 } 
 
