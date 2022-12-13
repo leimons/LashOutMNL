1548681a03191b646409c2feb2cb0ed8385d8902
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-12-13 21:04:28 +0800

    Update inclusion-controller.js

--- server/controllers/inclusion-controller.js
+++ server/controllers/inclusion-controller.js
@@ -3,14 +3,14 @@ const Inclusions = require('../database/models/Inclusions.js');
 
 const i_controller = {
 	i_add: function(req, res) {
-        console.log("inserting: " + req.query.Name);
-        db.insertOne(Inclusions, req.query, function(){}); 
-	console.log(req.query.Name + "added");
+        console.log("inserting: " + req.body.Name);
+        db.insertOne(Inclusions, req.body, function(){}); 
+	console.log(req.body.Name + "added");
 	res.status(201).send();
     	},
 	
 	i_delete: function (req, res) {
-	var InclusionsName = req.query.Name
+	var InclusionsName = req.body.Name
         console.log("deleting: " + InclusionsName);
         db.deleteOne(Inclusions,{Name:InclusionsName}, function(){});
 	console.log("Inclusions " + InclusionsName + " has been removed");
@@ -18,9 +18,9 @@ const i_controller = {
     	},
 	
 	i_edit: function(req, res) {
-	var ref = req.query._id;
-        console.log("updating: "+req.query.Name);
-        db.updateOne({_id:ref},{Name:req.query.Name},{Category: req.query.Category},{Price: req.query.Price});
+	var ref = req.body._id;
+        console.log("updating: "+req.body.Name);
+        db.updateOne({_id:ref},{Name:req.body.Name},{Category: req.body.Category},{Price: req.body.Price});
 	console.log(ref + " updated");
 	res.status(201).send();
 	}
