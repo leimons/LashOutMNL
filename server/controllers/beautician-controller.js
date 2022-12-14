7a04a64959c36c2d0f2af607c2bce05842568267
JPMPDev <95627276+JPMPDev@users.noreply.github.com>
2022-12-14 20:12:39 +0800

    Update beautician-controller.js

--- server/controllers/beautician-controller.js
+++ server/controllers/beautician-controller.js
@@ -5,24 +5,25 @@ const bea_controller = {
 	bea_add: function(req, res) {
         console.log("inserting: " + req.body.Name);
         db.insertOne(Beautician, req.body, function(){}); 
-	console.log(req.body.Name + "added");
-	res.status(201).send();
+		console.log(req.body.Name + "added");
+		res.status(201).send();
     	},
 	
 	bea_delete: function (req, res) {
-	var ref = req.body._id;
-	var beauticianName = req.body.Name
+		var ref = req.body._id;
+		var beauticianName = req.body.Name
         console.log("deleting: " + beauticianName);
         db.deleteOne(Beautician,{_id:ref}, function(){});
-	console.log("Beautician " + beauticianName + " has been removed");
-	res.status(201).send();
+		console.log("Beautician " + beauticianName + " has been removed");
+		res.status(201).send();
     	},
 	
 	bea_schedule_change: function(req, res) {
 	var ref = req.body._id;
-        console.log("updating: "+req.body.Name);
-        db.updateOne({_id:ref},{Name:req.body.name});
-	console.log(s_name + " updated");
+	var b_name = req.body.name;
+        console.log("updating: "+b_name);
+        db.updateOne({_id:ref},{Name:b_name, Services: req.body.Services});
+	console.log(b_name + " updated");
 	res.status(201).send();
 	}
 } 
